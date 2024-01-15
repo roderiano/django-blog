a0807a382b974f35fcc32bdad30129a2df84c92d
Gabriel Silveira <gabrielsilveirapro@gmail.com>
2024-01-15 11:14:57 -0300

    add search mechanism

--- post/views.py
+++ post/views.py
@@ -1,16 +1,16 @@
-from django.shortcuts import render
 from django.shortcuts import render, get_object_or_404
-from django.utils.safestring import mark_safe
-import markdown
-
-from django.shortcuts import render
-from .models import Post
-from tag.models import Tag
+from .models import Post, Tag
+from django.db.models import Q
 
 def post_list(request):
-    posts = Post.objects.all()
+    posts = Post.objects.all().order_by('-pub_date')
     tags = Tag.objects.all()
-    return render(request, 'post_list.html', {'posts': posts, 'tags': tags})
+
+    search_query = request.GET.get('q', '')
+    if search_query:
+        posts = posts.filter(Q(title__icontains=search_query) | Q(tag__name__icontains=search_query))
+
+    return render(request, 'post_list.html', {'posts': posts, 'tags': tags, 'search_query': search_query})
 
 def post_detail(request, post_id):
     post = get_object_or_404(Post, pk=post_id)
--- tag/models.py
+++ tag/models.py
@@ -3,5 +3,8 @@ from django.db import models
 class Tag(models.Model):
     name = models.CharField(max_length=100)
 
+    def post_amount(self):
+        return self.post_set.count()
+
     def __str__(self):
         return self.name
