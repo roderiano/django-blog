c309c67ab5bc1fe9f3d980d53110fbc1f012de42
Gabriel Silveira <gabrielsilveirapro@gmail.com>
2024-01-15 11:31:56 -0300

    add blog tests

--- post/tests.py
+++ post/tests.py
@@ -1,3 +1,43 @@
 from django.test import TestCase
 
-# Create your tests here.
+from django.test import TestCase
+from django.contrib.auth.models import User
+from post.models import Tag, Post
+from django.utils import timezone
+
+class PostModelTest(TestCase):
+    def setUp(self):
+        # Create a sample user for testing
+        self.user = User.objects.create_user(username='testuser', password='testpassword')
+
+        # Create a sample tag for testing
+        self.tag = Tag.objects.create(name='Test Tag')
+
+        # Create a sample post for testing
+        self.post = Post.objects.create(
+            title='Test Post',
+            content='This is a test post content.',
+            pub_date=timezone.now(),
+            tag=self.tag,
+            author=self.user
+        )
+
+    def test_post_title(self):
+        self.assertEqual(str(self.post), 'Test Post')
+
+    def test_post_content(self):
+        self.assertEqual(self.post.content, 'This is a test post content.')
+
+    def test_post_pub_date(self):
+        # Ensure pub_date is within a reasonable timeframe
+        self.assertLess(self.post.pub_date, timezone.now())
+
+    def test_post_tag(self):
+        self.assertEqual(self.post.tag, self.tag)
+
+    def test_post_author(self):
+        self.assertEqual(self.post.author, self.user)
+
+    def test_post_str_representation(self):
+        expected_str = f'{self.post.title}'
+        self.assertEqual(str(self.post), expected_str)
--- tag/tests.py
+++ tag/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from .models import Tag
 
-# Create your tests here.
+class TagModelTest(TestCase):
+    def setUp(self):
+        # Create sample tags for testing
+        Tag.objects.create(name='Tag1')
+        Tag.objects.create(name='Tag2')
+
+    def test_tag_name(self):
+        tag1 = Tag.objects.get(name='Tag1')
+        tag2 = Tag.objects.get(name='Tag2')
+        self.assertEqual(str(tag1), 'Tag1')
+        self.assertEqual(str(tag2), 'Tag2')
+
+    def test_tag_count(self):
+        tags_count = Tag.objects.count()
+        self.assertEqual(tags_count, 2)
