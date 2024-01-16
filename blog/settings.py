a69c0f8086a5524717a7de897a424f2b68d1f687
matt <matt@mattkingsbury.com>
2024-01-16 18:43:27 -0500

    DEBUG

--- blog/settings.py
+++ blog/settings.py
@@ -10,15 +10,14 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
-from pathlib import Path
 import os
+from pathlib import Path
 
 # Blog name
 BLOG_NAME = "Django Blog"
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
@@ -30,7 +29,6 @@ SECRET_KEY = 'django-insecure-ie8%(j3w8qyxv993=+l%7ty0e+m62mt^eyxi87e($1lzp+1^qk
 DEBUG = True
 
 ALLOWED_HOSTS = ['matt2.enterprisegradesecurity.com', 'mattkingsbury.com', 'www.mattkingsbury.com', 'localhost']
-
 
 # Application definition
 
@@ -78,7 +76,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'blog.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
@@ -88,7 +85,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
@@ -108,7 +104,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
@@ -120,7 +115,6 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
@@ -131,7 +125,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
 
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 
@@ -210,7 +203,7 @@ MARKDOWNIFY = {
             'href',
             'src',
             'alt',
-            ],
+        ],
         "WHITELIST_PROTOCOL": [
             'http',
             'https',
@@ -218,3 +211,7 @@ MARKDOWNIFY = {
         "BLEACH": True
     }
 }
+
+
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
